14f3cdd7dfe6ead1d3b167582ced450247850fbb
root <55458737+BryanCL01@users.noreply.github.com>
2025-10-28 11:25:01 -0700

    Game Code
    Initial base code for game

---
+++ TheBigGame/Source/TheBigGame/TheBigGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TheBigGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TheBigGame, "TheBigGame" );
